ba038f060e12d9815237e2ccff66b976698c98d4
Anna_Dmitrieva <catdogcat000gmail.com>
2022-11-03 03:33:13 +0300

    add S1_task2_Version2

---
+++ S1_task2_Version2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите два числа:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int sqr = num2*num2;
+
+if(sqr == num1)
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго");
+}

62d3b391b9d8035dd2f12d8d10d8b9c4e1ea434f
Anna_Dmitrieva <catdogcat000gmail.com>
2022-11-03 04:16:27 +0300

    add S1_task3_Weekdays

---
+++ S1_task3_Weekdays/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите число от 1 до 7");
+int num = Convert.ToInt32(Console.ReadLine());
+
+switch (num)
+{
+    case 1:
+        Console.WriteLine("Понедельник");
+        break;
+    case 2:
+        Console.WriteLine("Вторник");
+        break;
+    case 3:
+        Console.WriteLine("Среда");
+        break;
+    case 4:
+        Console.WriteLine("Четверг");
+        break;
+    case 5:
+        Console.WriteLine("Пятница");
+        break;
+    case 6:
+        Console.WriteLine("Суббота");
+        break;
+    case 7:
+        Console.WriteLine("Воскресенье");
+        break;    
+    default:
+        Console.WriteLine("Число больше 7 или меньше 1");
+        break;
+}

df6b6c47e03e7f5982b357496b42ae63a342f095
Anna_Dmitrieva <catdogcat000gmail.com>
2022-11-03 04:43:23 +0300

    add S1_task4_v1

---
+++ S1_task4_v1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N < 0)
+{
+    Console.WriteLine("Число отрицательное");
+}
+else
+{
+    int number = -N;
+
+    while (number != N + 1)
+    {
+        Console.Write(number + ", ");
+        number++;
+    }
+}

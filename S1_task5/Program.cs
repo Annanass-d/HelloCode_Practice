b15972a312186d7cfeea0be670f408479af8c3ae
Anna_Dmitrieva <catdogcat000gmail.com>
2022-11-03 05:24:06 +0300

    S1_task5

---
+++ S1_task5/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите трёхзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num>99 && num<1000)
+{
+    Console.WriteLine(num%10);
+}
+else
+{
+    Console.WriteLine("Число не трёхзначное");
+}

af18841aef20b961455900301625891ff08bac6f
Anna_Dmitrieva <catdogcat000gmail.com>
2022-11-04 10:35:35 +0300

    add S2_task2

---
+++ Seminar_2/S2_task2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+var random = new Random();
+
+int num = random.Next(100, 1000);
+Console.Write($"{num} -> ");
+
+int numFirst = num / 100;
+int numThird = num % 10;
+int result = numFirst * 10 + numThird;
+
+Console.WriteLine(result);

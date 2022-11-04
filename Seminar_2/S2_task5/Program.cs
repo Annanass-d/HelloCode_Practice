ad3781d32152aba45fc9aad01df89c6ab1f36e10
Anna_Dmitrieva <catdogcat000gmail.com>
2022-11-04 12:27:20 +0300

    add S2_task5

---
+++ Seminar_2/S2_task5/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+Console.WriteLine("Введите два числа:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+int min = num2;
+
+if (num2 > max)
+{
+    max = num2;
+    min = num1;
+}
+
+if (max == Math.Pow(min,2))
+{
+    Console.WriteLine($"Число {max} является квадратом {min}");
+}
+else
+{
+    Console.WriteLine($"Число {max} не является квадратом {min}");
+}

aeb84de47dd6cd57b073d9b4807d2e5e35a844da
Anna_Dmitrieva <catdogcat000gmail.com>
2022-11-04 10:10:15 +0300

    add S2_task1

---
+++ Seminar_2/S2_task1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10,99] и показывает наибольшую цифру числа.
+
+var random = new Random();
+
+int num = random.Next(10, 100);
+Console.WriteLine($"Число {num}");
+int numFirst = num / 10;
+int numSecond = num % 10;
+
+int max = numFirst;
+
+if (max < numSecond)
+{
+    max = numSecond;
+}
+Console.WriteLine($"Максимальная цифра в числе {num} -> {max}");

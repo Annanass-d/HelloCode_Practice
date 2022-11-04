2e7326e9170b6c3bb160ddf3d18ce897f2d5e4d8
Anna_Dmitrieva <catdogcat000gmail.com>
2022-11-04 11:12:15 +0300

    add S2_task3

---
+++ Seminar_2/S2_task3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число крастным первому.
+// Если число 2 не крастно чсилу 1, то программа выводит остаток деления.
+
+Console.WriteLine("Введите два числа:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int result = num1 % num2;
+
+if (result == 0)
+{
+    Console.WriteLine("-> кратно");
+}
+else
+{
+    Console.WriteLine($"-> не кратно, остаток {result}");
+}

59c35ef8b467d43dd9ab79191cf84e1dda54e34b
Anna_Dmitrieva <catdogcat000gmail.com>
2022-11-06 22:06:11 +0300

    add S3_Task1

---
+++ Seminar_3/S3_Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход координаты точки(X,Y), причем они не равны нулю,
+// и выдает номер четверти плоскости, в которой находится эта точка.
+
+static void ViewQuaterNumber()
+{
+    System.Console.WriteLine("Введите два числа (не равных нулю) - координаты точки:");
+    int X = Convert.ToInt32(Console.ReadLine());
+    int Y = Convert.ToInt32(Console.ReadLine());
+
+    if (X > 0 && Y > 0)
+    {
+        Console.WriteLine("Первая четверть");
+    }
+    if (X < 0 && Y > 0)
+    {
+        Console.WriteLine("Вторая четверть");
+    }
+    if (X < 0 && Y < 0)
+    {
+        Console.WriteLine("Третья четверть");
+    }
+    if (X > 0 && Y < 0)
+    {
+        Console.WriteLine("Четвертая четверть");
+    }
+}
+ViewQuaterNumber();

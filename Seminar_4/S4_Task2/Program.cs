6acf080858e62731673c591e135dfda56e0aa34e
Anna_Dmitrieva <catdogcat000gmail.com>
2022-11-08 16:47:40 +0300

    add S4_Task2

---
+++ Seminar_4/S4_Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход число и выдает количество цифр в числе.
+
+int SetNumber()
+{
+    System.Console.WriteLine("Введите число");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int NumberLength (int number)
+{
+    int count = number;
+    int i = 1;
+    while (count >10)
+    {
+        count /= 10;
+        i++;
+    }
+    return i;
+}
+
+int number = SetNumber();
+int result = NumberLength(number);
+System.Console.WriteLine(result);

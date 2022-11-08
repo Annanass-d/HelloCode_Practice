8ef91da2431289ce3ce21f13337b2b64b7e4980f
Anna_Dmitrieva <catdogcat000gmail.com>
2022-11-08 17:27:44 +0300

    add S4_Task3

---
+++ Seminar_4/S4_Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число N и выдает произведение чисел от 1 до N.
+
+int SetNumber()
+{
+    System.Console.WriteLine("Введите число");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int Multipl (int num)
+{
+    int product = 1;
+    for (int i = 1; i <=num; i++)
+    {
+        product = product * i;
+    }
+    return product;
+}
+
+int number = SetNumber();
+int result = Multipl(number);
+System.Console.WriteLine($"Произведение чисел от 1 до {number} = {result}");

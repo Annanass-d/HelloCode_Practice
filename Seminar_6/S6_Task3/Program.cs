cd46ff4ba0844a1d88f4da93e6c34ea8348efb3f
Anna_Dmitrieva <catdogcat000gmail.com>
2022-11-17 00:21:55 +0300

    add S6_Task3

---
+++ Seminar_6/S6_Task3/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+int GetNum(string str)
+{
+    System.Console.WriteLine($"Введите число {str}");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+string ConvertToBin(int num)
+{
+    string res = String.Empty;
+    for (int i = num; i > 0; i /= 2)
+    {
+        res = i % 2 + res;
+    }
+    return res;
+}
+
+// string ConvertToBin(int num)
+// {
+//     string res = String.Empty;
+//     while (num > 0)
+//     {
+//         res = num % 2 + res;
+//         num = num / 2;
+//     }
+//     return res;
+// }
+
+
+int number = GetNum("десятичное");
+string numberBin = ConvertToBin(number);
+System.Console.WriteLine($"{number} -> {numberBin}");
+
+
